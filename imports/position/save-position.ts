d71dd5ce28b571ec2d851f460318c09290ad4b3b
Eugen Soloviov <suenot@gmail.com>
2022-12-30 23:32:58 +0300

    save links with coordinates only if them are existing

--- imports/position/save-position.ts
+++ imports/position/save-position.ts
@@ -12,31 +12,39 @@ export async function savePosition(deep: DeepClient, deviceLinkId: number, coord
   const xTypeLinkId = await deep.id(PACKAGE_NAME, 'X');
   const yTypeLinkId = await deep.id(PACKAGE_NAME, 'Y');
   const zTypeLinkId = await deep.id(PACKAGE_NAME, 'Z');
+  console.log({coordinates});
+  
+  if (coordinates?.x) {
+    const {
+      data: [{ id: xLinkId }],
+    } = await deep.insert({
+      type_id: xTypeLinkId,
+      from_id: deviceLinkId,
+      to_id: geolocationEarthTypeLinkId,
+      string: { data: { value: (coordinates?.x || "").toString() } },
+    });
+  }
 
-	const {
-		data: [{ id: xLinkId }],
-	} = await deep.insert({
-		type_id: xTypeLinkId,
-    from_id: deviceLinkId,
-    to_id: geolocationEarthTypeLinkId,
-		number: { data: { value: coordinates?.x } },
-	});
+  if (coordinates?.y) {
+    const {
+      data: [{ id: yLinkId }],
+    } = await deep.insert({
+      type_id: yTypeLinkId,
+      from_id: deviceLinkId,
+      to_id: geolocationEarthTypeLinkId,
+      string: { data: { value: (coordinates?.y || "").toString() } },
+    });
+  }
 
-	const {
-		data: [{ id: yLinkId }],
-	} = await deep.insert({
-		type_id: yTypeLinkId,
-    from_id: deviceLinkId,
-    to_id: geolocationEarthTypeLinkId,
-		number: { data: { value: coordinates?.y } },
-	});
+  if (coordinates?.z) {
+    const {
+      data: [{ id: zLinkId }],
+    } = await deep.insert({
+      type_id: zTypeLinkId,
+      from_id: deviceLinkId,
+      to_id: geolocationEarthTypeLinkId,
+      string: { data: { value: (coordinates?.z || "").toString() } },
+    });
+  }
 
-  const {
-		data: [{ id: zLinkId }],
-	} = await deep.insert({
-		type_id: zTypeLinkId,
-    from_id: deviceLinkId,
-    to_id: geolocationEarthTypeLinkId,
-		number: { data: { value: coordinates?.z } },
-	});
 }
