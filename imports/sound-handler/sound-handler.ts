1b1aaf60e5ca7c80856d9819ea338cf5e392fc2d
Roman <technopriest666@gmail.com>
2023-03-12 01:45:13 +0300

    using gcloud credentials from link value

--- imports/sound-handler/sound-handler.ts
+++ imports/sound-handler/sound-handler.ts
@@ -10,42 +10,58 @@ export default async function insertSoundHandler(deep: DeepClient) {
   const handlerTypeLinkId = await deep.id("@deep-foundation/core", "Handler");
   const packageId = await deep.id("@deep-foundation/sound-handler");
   const handleOperationTypeLinkId = await deep.id("@deep-foundation/core", "HandleInsert");
-  const triggerTypeLinkId = await deep.id("@deep-foundation/audiorecord", "Sound");
+  const triggerTypeLinkId = await deep.id("@deep-foundation/audiorecord", "Record");
 
   const code = /*javascript*/`async ({ require, deep, data: { newLink } }) => {
     const speech = require('@google-cloud/speech');
     const fs = require('fs');
-
-    // const { data: [{ value: mimetypeLink }] } = await deep.select({
-    //   up: {
-    //     parent: {
-    //       id: newLink.id
-    //     },
-    //     link: {
-    //       type_id: await deep.id("@deep-foundation/audiorecord", "MIME/type")
-    //     }  
-    //   },
-    // })
-
-    // const makeTempDirectory = () => {
-    //   const os = require('os');
-    //   const { v4: uuid } = require('uuid');
-      
-    //   const baseTempDirectory = os.tmpdir();
-    //   const randomId = uuid();
-    //   const tempDirectory = [baseTempDirectory,randomId].join('/');
-    //   fs.mkdirSync(tempDirectory);
-    //   console.log(tempDirectory);
-    //   return tempDirectory;
-    // };
-
+    const os = require('os');
+    const { v4: uuid } = require('uuid');
+  
+    const soundTypelinkId = await deep.id("@deep-foundation/audiorecord", "Sound");
+    const mimetypeTypelinkId = await deep.id("@deep-foundation/audiorecord", "MIME/type");
+  
+    const { data } = await deep.select({
+      up: {
+        parent: {
+          id: newLink.id
+        },
+        link: {
+          type_id: {
+            _in:
+              [
+                soundTypelinkId,
+                mimetypeTypelinkId
+              ]
+          }
+        }
+      },
+    });
+  
+    const soundLink = data.filter((link) => link.type_id === soundTypelinkId)
+    const mimetypeLink = data.filter((link) => link.type_id === mimetypeTypelinkId)
+  
+    const authFilelinkId = await deep.id("@deep-foundation/sound-handler", "GoogleCloudAuthFile");
+    const { data: [{ value: { value: authFile } }] } = await deep.select({ type_id: authFilelinkId });
+  
+    const baseTempDirectory = os.tmpdir();
+    const randomId = uuid();
+    const tempDirectory = [baseTempDirectory, randomId].join('/');
+    fs.mkdirSync(tempDirectory);
+    const keyFilePath = \`\${tempDirectory}/key.json\`;
+    fs.writeFile(keyFilePath, JSON.stringify(authFile), (error) => {
+      if (error) throw error;
+    })
+  
+    process.env["GOOGLE_APPLICATION_CREDENTIALS"] = keyFilePath;
+  
     const client = new speech.SpeechClient();
-
+  
     const audio = {
-      content: newLink.value.value,
+      content: soundLink[0].value.value,
     };
     const config = {
-      encoding: 'WEBM_OPUS',
+      encoding: mimetypeLink[0].value.value === 'audio/webm;codecs=opus' ? 'WEBM_OPUS' : 'LINEAR16',
       sampleRateHertz: 48000,
       languageCode: 'ru-RU',
     };
@@ -53,14 +69,14 @@ export default async function insertSoundHandler(deep: DeepClient) {
       audio: audio,
       config: config,
     };
-
+  
     const [response] = await client.recognize(request);
     const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\\n');
-
+      .map(result => result.alternatives[0].transcript)
+      .join('\\n');
+  
     await deep.insert({
-      type_id: await deep.id("@deep-foundation/sound-handler", "GoogleSpeechTranscription"), 
+      type_id: await deep.id("@deep-foundation/sound-handler", "GoogleSpeechTranscription"),
       string: { data: { value: transcription } },
       in: {
         data: {
@@ -69,6 +85,8 @@ export default async function insertSoundHandler(deep: DeepClient) {
         }
       }
     })
+  
+    fs.rmSync(keyFilePath, { recursive: true, force: true });
   }`
 
   await deep.insert({
