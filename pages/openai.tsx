546ce23d815211c45e269f22e0b3f5f6fa0006f6
flakeed <vova.krivosheya.2004@gmail.com>
2023-03-18 21:23:33 +0000

    update page openai

--- pages/openai.tsx
+++ pages/openai.tsx
@@ -7,7 +7,6 @@ import {
   useDeep,
   useDeepSubscription,
 } from '@deep-foundation/deeplinks/imports/client';
-
 import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
 import { PACKAGE_NAME } from '../imports/openai/package-name';
 import { Provider } from '../imports/provider';
@@ -53,6 +52,50 @@ function Content() {
         }}
       >
         add userInputLinkId link
+      </Button>
+      <Button
+        onClick={async () => {
+          let makeActive = true;
+          if (makeActive) {
+            await deep.delete({
+              up: {
+                tree_id: { _eq: await deep.id("@deep-foundation/core", "containTree") },
+                parent: {
+                  type_id: { _id: ["@deep-foundation/core", "Contain"] },
+                  to: { type_id: await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey"), },
+                  from_id: deep.linkId
+                }
+              }
+            })
+          }
+          
+          await deep.insert({
+            type_id:  await deep.id(PACKAGE_NAME, "OpenAiApiKey"),
+            string: { data: { value: process.env.OPENAI_API_KEY }},
+            in: {
+              data: [
+                {
+                  type_id: await deep.id('@deep-foundation/core', "Contain"),
+                  from_id: deep.linkId,
+                },
+                makeActive && {
+                  type_id:  await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey"),
+                  from_id: deep.linkId,
+                  in: {
+                    data: [
+                      {
+                        type_id: await deep.id('@deep-foundation/core', "Contain"),
+                        from_id: deep.linkId,
+                      },
+                    ],
+                  },
+                }
+              ],
+            },
+          })
+        }}
+      >
+        add OpenAiApiKey and UsesOpenAiApiKey links
       </Button>
     </Stack>
   );
