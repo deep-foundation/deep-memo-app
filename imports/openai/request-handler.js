b043ca0b1039c88f3bde977a77d78c2b86a7b62b
flakeed <vova.krivosheya.2004@gmail.com>
2023-03-20 02:09:31 +0000

    add message logic for chatgpt

--- imports/openai/request-handler.js
+++ imports/openai/request-handler.js
@@ -1,9 +1,12 @@
 async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
     const PACKAGE_NAME=`@flakeed/chatgpt`
+    const PACKAGE_NAME_MESSAGING=`@flakeed/messaging`
     const {Configuration, OpenAIApi} = require("openai")
     const openAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "OpenAiApiKey")
     const usesOpenAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey")
-
+    const messageTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Message")
+    const authorTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Author")
+    const replyTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Reply")
 
     const {data: [linkWithStringValue]} = await deep.select({
         id: openAiRequestLink.to_id
@@ -37,6 +40,18 @@ async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
         model: "gpt-3.5-turbo",
         messages: [{role: "user", content: openAiPrompt}],
     })
+
+    const replyContent = response.data.choices[0].message.content;
+    await deep.insert({
+        type_id: messageTypeLinkId,
+        in: {
+            data: {
+                type_id: authorTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: 'OpenAI Bot' } },
+            }
+        },
+        string: { data: { value: replyContent } },
+    });
     return response.data;
 }
-
