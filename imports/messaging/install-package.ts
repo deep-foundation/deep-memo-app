f7829f7a2fe1ec2f90af15f32a7b1bfa3f98afa9
flakeed <vova.krivosheya.2004@gmail.com>
2023-03-20 00:36:13 +0000

    make package name as const

--- imports/messaging/install-package.ts
+++ imports/messaging/install-package.ts
@@ -1,6 +1,6 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 const { generateApolloClient } = require('@deep-foundation/hasura/client');
-import { PACKAGE_NAME } from './package-name';
+const PACKAGE_NAME=`@flakeed/messaging`
 require('dotenv').config();
 export async function installPackage () {
     const apolloClient = generateApolloClient({
