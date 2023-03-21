5eb89b52356d3f9522ea61a200015e0ee93d04ee
flakeed <vova.krivosheya.2004@gmail.com>
2023-03-21 18:04:06 +0000

    update handler

--- imports/openai/request-handler.js
+++ imports/openai/request-handler.js
@@ -1,57 +1,65 @@
-async ({data: {newLink:openAiRequestLink,triggeredByLinkId},deep,require}) => {
-    const PACKAGE_NAME=`@flakeed/chatgpt`
-    const PACKAGE_NAME_MESSAGING=`@flakeed/messaging`
-    const {Configuration, OpenAIApi} = require("openai")
-    const openAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "OpenAiApiKey")
-    const usesOpenAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey")
-    const messageTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Message")
-    const authorTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Author")
-    const replyTypeLinkId = await deep.id(PACKAGE_NAME_MESSAGING, "Reply")
-
-    const {data: [linkWithStringValue]} = await deep.select({
-        id: openAiRequestLink.to_id
-    })
-    if(!linkWithStringValue.value?.value){
-        throw new Error(`##${linkWithStringValue.id} must have a value`)
+async ({ data: { newLink: replyLink, triggeredByLinkId }, deep, require }) => {
+    const PACKAGE_NAME = `@flakeed/chatgpt`;
+    const { Configuration, OpenAIApi } = require("openai");
+    const openAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "OpenAiApiKey");
+    const usesOpenAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey");
+  
+    const { data: [linkWithStringValue] } = await deep.select({
+      id: replyLink.from_id,
+    });
+    if (!linkWithStringValue.value?.value) {
+      throw new Error(`##${linkWithStringValue.id} must have a value`);
     }
-    const openAiPrompt = linkWithStringValue.value.value
-
+    const openAiPrompt = linkWithStringValue.value.value;
+  
     const { data: [apiKeyLink] } = await deep.select({
-        type_id: openAiApiKeyTypeLinkId,
-        in: {
-          type_id: usesOpenAiApiKeyTypeLinkId,
-          from_id: triggeredByLinkId
-        }
-      });
-
-    if(!apiKeyLink){
-        throw new Error(`A link with type ##${openAiApiKeyTypeLinkId} is not found`)
+      type_id: openAiApiKeyTypeLinkId,
+      in: {
+        type_id: usesOpenAiApiKeyTypeLinkId,
+        from_id: triggeredByLinkId,
+      },
+    });
+  
+    if (!apiKeyLink) {
+      throw new Error(`A link with type ##${openAiApiKeyTypeLinkId} is not found`);
     }
-    if(!apiKeyLink.value?.value){
-        throw new Error(`##${apiKeyLink.id} must have a value`)
+    if (!apiKeyLink.value?.value) {
+      throw new Error(`##${apiKeyLink.id} must have a value`);
     }
-    const apiKey = apiKeyLink.value.value
+    const apiKey = apiKeyLink.value.value;
     const configuration = new Configuration({
-        apiKey: apiKey,
+      apiKey: apiKey,
     });
     const openai = new OpenAIApi(configuration);
-
+  
     const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: openAiPrompt}],
-    })
-
-    const replyContent = response.data.choices[0].message.content;
-    await deep.insert({
-        type_id: messageTypeLinkId,
-        in: {
-            data: {
-                type_id: authorTypeLinkId,
-                from_id: packageLinkId,
-                string: { data: { value: 'OpenAI Bot' } },
-            }
-        },
-        string: { data: { value: replyContent } },
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: openAiPrompt }],
     });
+  
+    const { data: [{ id: agentMessageId }] } = await deep.insert({
+      type_id: await deep.id('@flakeed/messaging', "Message"),
+      string: { data: { value: response.data.choices[0].text.trim() } },
+      in: {
+        data: {
+          type_id: await deep.id('@deep-foundation/core', "Contain"),
+          from_id: deep.linkId,
+        },
+      },
+    });
+
+    const { data: [{ id: agentReplyLinkId }] } = await deep.insert({
+      type_id: await deep.id('@flakeed/messaging', "Reply"),
+      from_id: replyLink.from_id,
+      to_id: newAgentMessageInstanceId,
+      in: {
+        data: {
+          type_id: await deep.id('@deep-foundation/core', "Contain"),
+          from_id: deep.linkId,
+        },
+      },
+    });
+  
     return response.data;
-}
+  };
+  
