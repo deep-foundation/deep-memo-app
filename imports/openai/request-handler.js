4a2ffb6ca4369ba5cdfcddb30c4116b0fe2e9738
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-09 18:40:31 +0000

    not working code

--- imports/openai/request-handler.js
+++ imports/openai/request-handler.js
@@ -1,186 +1,232 @@
 async ({ data: { newLink: replyLink, triggeredByLinkId }, deep, require }) => {
-  const PACKAGE_NAME = `@flakeed/chatgpt`;
-  const { Configuration, OpenAIApi } = require("openai");
-  const openAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "OpenAiApiKey");
-  const usesOpenAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, "UsesOpenAiApiKey");
-  const messageTypeLinkId = await deep.id('@flakeed/messaging', "Message");
-  const replyTypeLinkId = await deep.id('@flakeed/messaging', "Reply");
-  const authorTypeLinkId = await deep.id('@flakeed/messaging', "Author");
-  const chatgptTypeLinkId = await deep.id(PACKAGE_NAME, "ChatGPT");
-  const containTypeLinkId = await deep.id('@deep-foundation/core', "Contain");
-  const modelTypeLinkId = await deep.id(PACKAGE_NAME, "Model");
-  const usesModelTypeLinkId = await deep.id(PACKAGE_NAME, "UsesModel");
-  const conversationTypeLinkId = await deep.id(PACKAGE_NAME, "Conversation");
-  const messagingTree = await deep.id('@flakeed/messaging', "MessagingTree");
-  let model;
+	const PACKAGE_NAME = `@flakeed/chatgpt`;
+	const { Configuration, OpenAIApi } = require('openai');
+	const openAiApiKeyTypeLinkId = await deep.id(PACKAGE_NAME, 'OpenAiApiKey');
+	const usesOpenAiApiKeyTypeLinkId = await deep.id(
+		PACKAGE_NAME,
+		'UsesOpenAiApiKey'
+	);
+	const messageTypeLinkId = await deep.id('@flakeed/messaging', 'Message');
+	const replyTypeLinkId = await deep.id('@flakeed/messaging', 'Reply');
+	const authorTypeLinkId = await deep.id('@flakeed/messaging', 'Author');
+	const chatgptTypeLinkId = await deep.id(PACKAGE_NAME, 'ChatGPT');
+	const containTypeLinkId = await deep.id('@deep-foundation/core', 'Contain');
+	const modelTypeLinkId = await deep.id(PACKAGE_NAME, 'Model');
+	const usesModelTypeLinkId = await deep.id(PACKAGE_NAME, 'UsesModel');
+	const conversationTypeLinkId = await deep.id(PACKAGE_NAME, 'Conversation');
+	const messagingTree = await deep.id('@flakeed/messaging', 'MessagingTree');
+	let model;
 
-  const { data: [messageLink = undefined] = [] } = await deep.select({
-    id: replyLink.from_id,
-    _not: {
-      out: {
-        to_id: chatgptTypeLinkId,
-        type_id: authorTypeLinkId
-      }
-    }
-  });
-  if (!messageLink) {
-    return 'No need to react to message of this reply.';
-  }
-  if (!messageLink.value?.value) {
-    throw new Error(`##${messageLink.id} must have a value`);
-  }
-  const message = messageLink.value.value;
+	const { data: [messageLink ] } = await deep.select({
+		id: replyLink.from_id,
+		_not: {
+			out: {
+				to_id: chatgptTypeLinkId,
+				type_id: authorTypeLinkId,
+			},
+		},
+	});
+	if (!messageLink) {
+		return 'No need to react to message of this reply.';
+	}
+	if (!messageLink.value?.value) {
+		throw new Error(`##${messageLink.id} must have a value`);
+	}
+	const message = messageLink.value.value;
 
-  const apiKey = getTokenLink();
+	const apiKeyLink = await getTokenLink();
+	const apiKey = apiKeyLink.value.value;
+	const configuration = new Configuration({
+		apiKey: apiKey,
+	});
+	const openai = new OpenAIApi(configuration);
 
-  const configuration = new Configuration({
-    apiKey: apiKey,
-  });
-  const openai = new OpenAIApi(configuration);
+	const { data: conversationLink } = await deep.select({
+		down: {
+			tree_id: { _eq: messagingTree },
+			link_id: { _eq: replyLink.id },
+		},
+	});
 
-  const { data: conversationLink } = await deep.select({
-    down: {
-      tree_id: { _eq: messagingTree },
-      link_id: { _eq: replyLink.id },
-    },
-  });
+	if (!conversationLink) {
+		throw new Error('A conversationLink link is not found');
+	}
+	const currentConversation = conversationLink.find(
+		(link) => link.type_id === conversationTypeLinkId
+	);
 
-  if (!conversationLink) {
-    throw new Error('A conversationLink link is not found');
-  }
-  const currentConversation=conversationLink.find((link) => link.type_id === conversationTypeLinkId);
+	const {
+		data: [linkedModel],
+	} = await deep.select({
+		type_id: modelTypeLinkId,
+		in: {
+			type_id: usesModelTypeLinkId,
+			from_id: currentConversation.id,
+		},
+	});
 
-  const { data: [linkedModel] } = await deep.select({
-  type_id: modelTypeLinkId,
-  in: {
-    type_id: usesModelTypeLinkId,
-    from_id: currentConversation.id, 
-    },
-  }); 
+	const {
+		data: [userLinkedModel],
+	} = await deep.select({
+		type_id: modelTypeLinkId,
+		in: {
+			type_id: usesModelTypeLinkId,
+			from_id: triggeredByLinkId,
+		},
+	});
 
-  const { data: [userLinkedModel] } = await deep.select({
-    type_id: modelTypeLinkId,
-    in: {
-      type_id: usesModelTypeLinkId,
-      from_id: triggeredByLinkId,
-    },
-  });
-
-  if (linkedModel && linkedModel.value?.value && userLinkedModel && userLinkedModel.value?.value || linkedModel && linkedModel.value?.value) {
-    model = linkedModel.value.value;
-  } else {
-    if (!userLinkedModel) {
-      throw new Error(`A link with type ##${userLinkedModel} is not found`);
-    }
-    if (!userLinkedModel.value?.value) {
-      throw new Error(`##${userLinkedModel.id} must have a value`);
-    } else {
-      model = userLinkedModel.value.value;
-    }
-  }
-  if (!model) {
-    model="gpt-3.5-turbo";
-  }
-
-  const messageLinks = conversationLink.filter(link => link.type_id === messageTypeLinkId);
-    
+	if (
+		(linkedModel &&
+			linkedModel.value?.value &&
+			userLinkedModel &&
+			userLinkedModel.value?.value) ||
+		(linkedModel && linkedModel.value?.value)
+	) {
+		model = linkedModel.value.value;
+	} else {
+		if (!userLinkedModel) {
+			throw new Error(`A link with type ##${userLinkedModel} is not found`);
+		}
+		if (!userLinkedModel.value?.value) {
+			throw new Error(`##${userLinkedModel.id} must have a value`);
+		} else {
+			model = userLinkedModel.value.value;
+		}
+	}
+	if (!model) {
+		model = 'gpt-3.5-turbo';
+	}
+  const messageLinks = conversationLink.filter((link) => link.type_id === messageTypeLinkId);
+console.log("messageLinks",messageLinks)
+  const allMessages = await getMessages({ messageLinks: messageLinks });
+console.log("allMessages",allMessages)
   const response = await openai.createChatCompletion({
     model: model,
-    messages: messageLinks.map((link) => ({
-        role: getMessageRole({ messageLink: link }),
-        content: link.value.value,
-    })),
-  });
-
-  
-  const { data: [{ id: chatgptMessageLinkId }] } = await deep.insert({
-  type_id: messageTypeLinkId,
-  string: { data: { value: response.data.choices[0].message.content } },
-  in:{
-    data: [
-      {
-        type_id: containTypeLinkId,
-        from_id: triggeredByLinkId,
-      },
+    messages: [
+        ...allMessages,
+        {
+            role: 'user',
+            content: message,
+        },
     ],
-  },
-  out: {
-    data: [
-      {
-        type_id: authorTypeLinkId,
-        to_id: chatgptTypeLinkId,
-      },
-    ],
-  },
 });
 
-  const { data: [{ id: replyToMessageLinkId }] } = await deep.insert({
-    type_id: replyTypeLinkId,
-    from_id: chatgptMessageLinkId,
-    to_id: replyLink.from_id,
-    in: {
-      data: {
-        type_id: containTypeLinkId,
-        from_id: triggeredByLinkId,
+console.log("reponse ",{
+  model: model,
+  messages: [
+      ...allMessages,
+      {
+          role: 'user',
+          content: message,
       },
-    },
-  });
-  async function getTokenLink() {
-    let resultTokenLink;
-    const { data } = await deep.select({
-        _or: [
-            {
-                type_id: openAiApiKeyTypeLinkId,
-                in: {
-                    type_id: containTypeLinkId,
-                    from_id: triggeredByLinkId,
-                },
-            },
-            {
-                from_id: triggeredByLinkId,
-                type_id: usesOpenAiApiKeyTypeLinkId,
-            },
-        ],
-    });
-    if (data.length === 0) {
-        throw new Error(`Link of type ##${openAiApiKeyTypeLinkId} is not found`);
-    }
-    const usesLinks = data.filter(
-        (link) => link.type_id === usesOpenAiApiKeyTypeLinkId
-    );
-    if (usesLinks.length > 1) {
-        throw new Error(
-            `More than 1 links of type ##${usesOpenAiApiKeyTypeLinkId} are found`
-        );
-    }
-    const usesLink = data.find(
-        (link) => link.type_id === usesOpenAiApiKeyTypeLinkId
-    );
-    if (usesLink) {
-        const tokenLink = data.find((link) => link.id === usesLink.to_id);
-        resultTokenLink = tokenLink;
-    } else {
-        const tokenLink = data.find(
-            (link) => link.type_id === openAiApiKeyTypeLinkId
-        );
-        if (!tokenLink) {
-            throw new Error(`Link of type ${openAiApiKeyTypeLinkId} is not found`);
-        }
-        resultTokenLink = tokenLink;
-    }
-    if (!resultTokenLink.value?.value) {
-        throw new Error(`Link of type ${openAiApiKeyTypeLinkId} has no value`);
-    }
-    return resultTokenLink;
-}
+  ],
+})
+console.log("messageLinks",messageLinks);
+	const {
+		data: [{ id: chatgptMessageLinkId }],
+	} = await deep.insert({
+		type_id: messageTypeLinkId,
+		string: { data: { value: response.data.choices[0].message.content } },
+		in: {
+			data: [
+				{
+					type_id: containTypeLinkId,
+					from_id: triggeredByLinkId,
+				},
+			],
+		},
+		out: {
+			data: [
+				{
+					type_id: authorTypeLinkId,
+					to_id: chatgptTypeLinkId,
+				},
+			],
+		},
+	});
 
-async function getMessageRole({ messageLink }) {
-  const authorLink = messageLinks.find(
+	const {
+		data: [{ id: replyToMessageLinkId }],
+	} = await deep.insert({
+		type_id: replyTypeLinkId,
+		from_id: chatgptMessageLinkId,
+		to_id: replyLink.from_id,
+		in: {
+			data: {
+				type_id: containTypeLinkId,
+				from_id: triggeredByLinkId,
+			},
+		},
+	});
+
+	async function getMessages({ messageLinks }) {
+    return Promise.all(
+        messageLinks.map(async (link) => ({
+            role: await getMessageRole({ messageLink: link }),
+            content: link.value.value,
+        }))
+    );
+}
+	async function getTokenLink() {
+		let resultTokenLink;
+		const { data } = await deep.select({
+			_or: [
+				{
+					type_id: openAiApiKeyTypeLinkId,
+					in: {
+						type_id: containTypeLinkId,
+						from_id: triggeredByLinkId,
+					},
+				},
+				{
+					from_id: triggeredByLinkId,
+					type_id: usesOpenAiApiKeyTypeLinkId,
+				},
+			],
+		});
+		if (data.length === 0) {
+			throw new Error(`Link of type ##${openAiApiKeyTypeLinkId} is not found`);
+		}
+		const usesLinks = data.filter(
+			(link) => link.type_id === usesOpenAiApiKeyTypeLinkId
+		);
+		if (usesLinks.length > 1) {
+			throw new Error(
+				`More than 1 links of type ##${usesOpenAiApiKeyTypeLinkId} are found`
+			);
+		}
+		const usesLink = data.find(
+			(link) => link.type_id === usesOpenAiApiKeyTypeLinkId
+		);
+		if (usesLink) {
+			const tokenLink = data.find((link) => link.id === usesLink.to_id);
+			resultTokenLink = tokenLink;
+		} else {
+			const tokenLink = data.find(
+				(link) => link.type_id === openAiApiKeyTypeLinkId
+			);
+			if (!tokenLink) {
+				throw new Error(`Link of type ${openAiApiKeyTypeLinkId} is not found`);
+			}
+			resultTokenLink = tokenLink;
+		}
+		if (!resultTokenLink.value?.value) {
+			throw new Error(`Link of type ${openAiApiKeyTypeLinkId} has no value`);
+		}
+		return resultTokenLink;
+	}
+
+	async function getMessageRole({ messageLink }) {
+    const authorLink = messageLinks.find(
       (link) =>
-          link.type_id === authorTypeLinkId && link.from_id === messageLink.id
-  );
+        link.type_id === authorTypeLinkId && link.from_id === messageLink.id
+    );
+    console.log("authorLink",authorLink);
+    if (!authorLink) {
+      throw new Error(`Author link not found for message ##${messageLink.id}`);
+    }
     return authorLink.to_id === chatgptTypeLinkId ? 'assistant' : 'user';
   }
 
-  return response.data;
+	return response.data;
 };
