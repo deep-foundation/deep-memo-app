6281c90273d2fe163ffd711aec129cacd522b7a0
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-12 16:45:51 +0000

    add all into one button

--- pages/chatgpt.tsx
+++ pages/chatgpt.tsx
@@ -1,3 +1,5 @@
+
+
 import {
   LocalStoreProvider,
   useLocalStore,
@@ -8,9 +10,8 @@ import {
   useDeepSubscription,
 } from '@deep-foundation/deeplinks/imports/client';
 import { Button, ChakraProvider, Stack, Text } from '@chakra-ui/react';
-import { PACKAGE_NAME } from '../imports/chatgpt/package-name';
 import { Provider } from '../imports/provider';
-
+const PACKAGE_NAME = "@deep-foundation/openai";
 function Content() {
   const deep = useDeep();
   const [openAiLinkId, setOpenAiLinkId] = useLocalStore(
@@ -24,7 +25,7 @@ function Content() {
       <Button
         onClick={async () => {
           const { data: [{ id: newConversationLinkId }] } = await deep.insert({
-            type_id: await deep.id(PACKAGE_NAME, "Conversation"),
+            type_id: await deep.id('@deep-foundation/chatgpt', "Conversation"),
             string: { data: { value: "New chat" } },
             in: {
               data: {
@@ -34,28 +35,82 @@ function Content() {
             },
           });
 
-          // const { data: [{ id: newMessageLinkId }] } = await deep.insert({
-          //   type_id: await deep.id('@flakeed/messaging', "Message"),
-          //   string: { data: { value: "message" } },
-          //   in: {
-          //     data: {
-          //       type_id: await deep.id('@deep-foundation/core', "Contain"),
-          //       from_id: deep.linkId,
-          //     },
-          //   },
-          // });
+          const { data: [{ id: newMessageLinkId }] } = await deep.insert({
+            type_id: await deep.id('@deep-foundation/messaging', "Message"),
+            string: { data: { value: "Who are you?" } },
+            in: {
+              data: {
+                type_id: await deep.id('@deep-foundation/core', "Contain"),
+                from_id: deep.linkId,
+              },
+            },
+          });
 
-          // const { data: [{ id: replyLinkId }] } = await deep.insert({
-          //   type_id: await deep.id('@flakeed/messaging', "Reply"),
-          //   from_id: await deep.id('@flakeed/messaging', "Message"),
-          //   to_id: newConversationLinkId,
-          //   in: {
-          //     data: {
-          //       type_id: await deep.id('@deep-foundation/core', "Contain"),
-          //       from_id: deep.linkId,
-          //     },
-          //   },
-          // });
+          let makeActive = true;
+          if (makeActive) {
+            await deep.delete({
+              up: {
+                tree_id: { _eq: await deep.id("@deep-foundation/core", "containTree") },
+                parent: {
+                  type_id: { _id: ["@deep-foundation/core", "Contain"] },
+                  to: { type_id: await deep.id(PACKAGE_NAME, "UsesApiKey"), },
+                  from_id: deep.linkId
+                }
+              }
+            })
+          }
+
+          await deep.insert({
+            type_id:  await deep.id(PACKAGE_NAME, "ApiKey"),
+            string: { data: { value: "" }},
+            in: {
+              data: [
+                {
+                  type_id: await deep.id('@deep-foundation/core', "Contain"),
+                  from_id: deep.linkId,
+                },
+                makeActive && {
+                  type_id:  await deep.id(PACKAGE_NAME, "UsesApiKey"),
+                  from_id: deep.linkId,
+                  in: {
+                    data: [
+                      {
+                        type_id: await deep.id('@deep-foundation/core', "Contain"),
+                        from_id: deep.linkId,
+                      },
+                    ],
+                  },
+                }
+              ],
+            },
+          })
+
+          const { data: [{ id: gpt_3_5_turbo_0301 }] } = await deep.insert({
+            type_id: await deep.id(PACKAGE_NAME, "Model"),
+            string: { data: { value: 'gpt-3.5-turbo-0301' } },
+            in: {
+              data: [
+                {
+                  type_id: await deep.id('@deep-foundation/core', "Contain"),
+                  from_id: deep.linkId,
+                  string: { data: { value: 'GPT 3.5 TURBO 0301' } },
+                },
+              ],
+            },
+          });
+
+            const { data: [{ id: usesModelLinkId }] } = await deep.insert({
+              type_id: await deep.id(PACKAGE_NAME,"UsesModel"),
+              from_id: newConversationLinkId,
+              to_id: gpt_3_5_turbo_0301,
+              in: {
+                  data: {
+                      type_id: await deep.id('@deep-foundation/core', "Contain"),
+                      from_id: deep.linkId,
+                      string: { data: { value: "UsesModel" } },
+                  },
+              },
+          });
         }
       }
       >
@@ -75,23 +130,7 @@ function Content() {
         });
         }}
       >
-        add openAiApiKeyLinkId link
-      </Button>
-      <Button
-        onClick={async () => {
-          await deep.insert({
-            type_id: await deep.id('@deep-foundation/core', "SyncTextFile"),
-            string: { data: { value: "user input" } },
-            in: {
-              data: {
-                type_id: await deep.id('@deep-foundation/core', "Contain"),
-                from_id: deep.linkId,
-              },
-            },
-          });
-        }}
-      >
-        add userInputLinkId link
+        add ApiKeyLinkId link
       </Button>
       <Button
         onClick={async () => {
@@ -152,3 +191,5 @@ export default function OpenaiPage() {
     </ChakraProvider>
   );
 }
+
+
