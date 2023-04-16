6372391c72a0eb517d71cf079275256802ca602c
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-16 13:18:17 +0000

    update auth file

--- imports/google-vision/auth-file.ts
+++ imports/google-vision/auth-file.ts
@@ -1,13 +1,13 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import { PACKAGE_NAME } from './install-package';
 
-export default async function insertGcloudAuthFile(deep: DeepClient, credentials) {
+export default async function insertGoogleCloudAuthFile(deep: DeepClient, credentials) {
   const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
-  const gcloudAuthKeyTypeLink = await deep.id(PACKAGE_NAME, "GoogleCloudAuthFile");
+  const googleCloudAuthKeyTypeLink = await deep.id(PACKAGE_NAME, "GoogleCloudAuthFile");
   const userLink = await deep.id('deep', 'admin');
 
-  const { data: [{ id: gcloudAuthKeyLinkId }] } = await deep.insert({
-    type_id: gcloudAuthKeyTypeLink,
+  const { data: [{ id: googleCloudAuthKeyLinkId }] } = await deep.insert({
+    type_id: googleCloudAuthKeyTypeLink,
     object: { data: { value: credentials } },
     in: {
       data: [
