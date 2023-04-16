69a50aea8888045dde8117586fc9f7c36200a16e
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-16 01:31:59 +0000

    add handler

--- imports/google-vision/media-handler.js
+++ imports/google-vision/media-handler.js
@@ -1,4 +1,73 @@
-async ({ data: { newLink: asyncFileLink, triggeredByLinkId }, deep, require }) => {
-	const PACKAGE_NAME = `@flakeed/google-vision`;
+async ({ require, deep: DeepClient, data: { newLink } }) => {
+  const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
+  const packageLinkId = await deep.id("@flakeed/google-vision");
 
-};
+  const vision = require('@google-cloud/vision');
+  const fs = require('fs');
+  const os = require('os');
+  const { v4: uuid } = require('uuid');
+
+  const videoTypelinkId = await deep.id("@flakeed/google-vision", "Video");
+  const photoTypelinkId = await deep.id("@flakeed/google-vision", "Photo");
+
+  // const { data } = await deep.select({
+
+  // });
+
+  // const soundLink = data.filter((link) => link.type_id === soundTypelinkId)
+  // const mimetypeLink = data.filter((link) => link.type_id === mimetypeTypelinkId)
+
+  const authFilelinkId = await deep.id("@flakeed/google-vision", "GoogleCloudAuthFile");
+  const { data: [{ value: { value: authFile } }] } = await deep.select({ type_id: authFilelinkId });
+
+  const baseTempDirectory = os.tmpdir();
+  const randomId = uuid();
+  const tempDirectory = [baseTempDirectory, randomId].join('/');
+  fs.mkdirSync(tempDirectory);
+  const keyFilePath = `${tempDirectory}/key.json`;
+  fs.writeFileSync(keyFilePath, JSON.stringify(authFile));
+  
+    try {
+      process.env["GOOGLE_APPLICATION_CREDENTIALS"] = keyFilePath;
+  
+      const client = new vision.ImageAnnotatorClient();
+
+      const request = {
+        "requests": [
+          {
+            "image": {
+              "source": {
+                "imageUri": ""
+              }
+            },
+            "features": [
+              {
+                "type": "TEXT_DETECTION"
+              }
+            ],
+            "imageContext": {
+              "languageHints": ["en"]
+            }
+          }
+        ]
+      };
+    
+      const [result] = await client.batchAnnotateImages(request);
+      const detections = result.responses[0].fullTextAnnotation;
+      console.log(detections.text);
+    
+      await deep.insert({
+        type_id: await deep.id("@flakeed/google-vision", "PhotoTranscription"),
+        string: { data: { value: detections } },
+        in: {
+          data: {
+            type_id: await deep.id("@deep-foundation/core", "Contain"),
+            from_id: newLink.to_id
+          }
+        }
+      })
+    } 
+    finally {
+      fs.rmSync(keyFilePath, { recursive: true, force: true });
+    }
+}
