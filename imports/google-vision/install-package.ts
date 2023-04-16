449f8cb0c92d97a343d88f390ebbd63c0aa27060
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-16 13:17:08 +0000

    update intsall package

--- imports/google-vision/install-package.ts
+++ imports/google-vision/install-package.ts
@@ -7,53 +7,53 @@ export const PACKAGE_NAME = "@flakeed/google-vision"
 export const PACKAGE_TYPE = "GoogleCloudAuthFile"
 
 export default async function installPackage() {
-    const apolloClient = generateApolloClient({
-      path: process.env.NEXT_PUBLIC_GQL_PATH || '',
-      ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
-        ? false
-        : true,
-    });
+  const apolloClient = generateApolloClient({
+    path: process.env.NEXT_PUBLIC_GQL_PATH || '',
+    ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost')
+      ? false
+      : true,
+  });
 
-    const unloginedDeep = new DeepClient({ apolloClient });
-    const guest = await unloginedDeep.guest();
-    const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
-    const admin = await guestDeep.login({
-      linkId: await guestDeep.id('deep', 'admin'),
-    });
-    const deep = new DeepClient({ deep: guestDeep, ...admin });
+  const unloginedDeep = new DeepClient({ apolloClient });
+  const guest = await unloginedDeep.guest();
+  const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+  const admin = await guestDeep.login({
+    linkId: await guestDeep.id('deep', 'admin'),
+  });
+  const deep = new DeepClient({ deep: guestDeep, ...admin });
 
-    const typeTypeLinkId = await deep.id('@deep-foundation/core', 'Type');
-    const containTypeLinkId = await deep.id('@deep-foundation/core', 'Contain');
-    const packageTypeLinkId = await deep.id('@deep-foundation/core', 'Package');
-    const joinTypeLinkId = await deep.id('@deep-foundation/core', 'Join');
-    const valueTypeLinkId = await deep.id('@deep-foundation/core', 'Value');
-    const stringTypeLinkId = await deep.id('@deep-foundation/core', 'String');
-    const fileTypeLinkId = await deep.id("@deep-foundation/core", "SyncTextFile");
-    const supportsId = await deep.id("@deep-foundation/core", "dockerSupportsJs");
-    const handlerTypeLinkId = await deep.id("@deep-foundation/core", "Handler");
-    const handleOperationTypeLinkId = await deep.id("@deep-foundation/core", "HandleInsert");
+  const typeTypeLinkId = await deep.id('@deep-foundation/core', 'Type');
+  const containTypeLinkId = await deep.id('@deep-foundation/core', 'Contain');
+  const packageTypeLinkId = await deep.id('@deep-foundation/core', 'Package');
+  const joinTypeLinkId = await deep.id('@deep-foundation/core', 'Join');
+  const valueTypeLinkId = await deep.id('@deep-foundation/core', 'Value');
+  const stringTypeLinkId = await deep.id('@deep-foundation/core', 'String');
+  const fileTypeLinkId = await deep.id("@deep-foundation/core", "SyncTextFile");
+  const supportsId = await deep.id("@deep-foundation/core", "dockerSupportsJs");
+  const handlerTypeLinkId = await deep.id("@deep-foundation/core", "Handler");
+  const handleOperationTypeLinkId = await deep.id("@deep-foundation/core", "HandleInsert");
 
-    const { data: [{ id: packageLinkId }] } = await deep.insert({
-      type_id: packageTypeLinkId,
-      string: { data: { value: PACKAGE_NAME } },
-      in: {
-        data: [{
-          type_id: containTypeLinkId,
-          from_id: deep.linkId,
-        }]
-      },
-      out: {
-        data: [{
-          type_id: joinTypeLinkId,
-          to_id: await deep.id('deep', 'users', 'packages')
-        }, {
-          type_id: joinTypeLinkId,
-          to_id: await deep.id('deep', 'admin')
-        }]
-      },
-    })
+  const { data: [{ id: packageLinkId }] } = await deep.insert({
+    type_id: packageTypeLinkId,
+    string: { data: { value: PACKAGE_NAME } },
+    in: {
+      data: [{
+        type_id: containTypeLinkId,
+        from_id: deep.linkId,
+      }]
+    },
+    out: {
+      data: [{
+        type_id: joinTypeLinkId,
+        to_id: await deep.id('deep', 'users', 'packages')
+      }, {
+        type_id: joinTypeLinkId,
+        to_id: await deep.id('deep', 'admin')
+      }]
+    },
+  })
 
-    const { data: [{ id: packageLinks }] } = await deep.insert([
+  const { data: [{ id: packageLinks }] } = await deep.insert([
     {
       type_id: typeTypeLinkId,
       in: {
@@ -79,49 +79,49 @@ export default async function installPackage() {
     }
   ]);
 
-    await deep.insert({
-      type_id: fileTypeLinkId,
-      in: {
-        data: [
-          {
-            type_id: containTypeLinkId,
-            from_id: packageLinkId,
-            string: { data: { value: "AsyncFileHandlerCode" } },
-          },
-          {
-            from_id: supportsId,
-            type_id: handlerTypeLinkId,
-            in: {
-              data: [
-                {
-                  type_id: containTypeLinkId,
-                  from_id: packageLinkId,
-                  string: { data: { value: "AsyncFileHandler" } },
-                },
-                {
-                  type_id: handleOperationTypeLinkId,
-                  from_id: await deep.id("@deep-foundation/core", "AsyncFile"),
-                  in: {
-                    data: [
-                      {
-                        type_id: containTypeLinkId,
-                        from_id: packageLinkId,
-                        string: { data: { value: "HandleAsyncFile" } },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      },
-      string: {
-        data: {
-          value: fs.readFileSync('/workspace/dev/packages/sdk/imports/google-vision/media-handler.js', { encoding: 'utf-8' }),
+  await deep.insert({
+    type_id: fileTypeLinkId,
+    in: {
+      data: [
+        {
+          type_id: containTypeLinkId,
+          from_id: packageLinkId,
+          string: { data: { value: "AsyncFileHandlerCode" } },
         },
+        {
+          from_id: supportsId,
+          type_id: handlerTypeLinkId,
+          in: {
+            data: [
+              {
+                type_id: containTypeLinkId,
+                from_id: packageLinkId,
+                string: { data: { value: "AsyncFileHandler" } },
+              },
+              {
+                type_id: handleOperationTypeLinkId,
+                from_id: await deep.id("@deep-foundation/core", "AsyncFile"),
+                in: {
+                  data: [
+                    {
+                      type_id: containTypeLinkId,
+                      from_id: packageLinkId,
+                      string: { data: { value: "HandleAsyncFile" } },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+    string: {
+      data: {
+        value: fs.readFileSync('/workspace/dev/packages/sdk/imports/google-vision/media-handler.js', { encoding: 'utf-8' }),
       },
-    });
+    },
+  });
 }
 
 installPackage();
