5c605772c1fd451226070680420c22bfbf9182e1
flakeed <vova.krivosheya.2004@gmail.com>
2023-04-17 23:06:46 +0000

    code with correct request into api

--- imports/google-vision/media-handler.js
+++ imports/google-vision/media-handler.js
@@ -54,49 +54,55 @@ async ({ data: { newLink }, deep, require }) => {
   fs.mkdirSync(tempDirectory);
   const keyFilePath = `${tempDirectory}/key.json`;
   fs.writeFileSync(keyFilePath, JSON.stringify(authFile));
-  
-    try {
-      process.env["GOOGLE_APPLICATION_CREDENTIALS"] = keyFilePath;
-  
-      const client = new vision.ImageAnnotatorClient();
+   let detections;
+   try {
+  process.env["GOOGLE_APPLICATION_CREDENTIALS"] = keyFilePath;
 
-      const request = {
-        "requests": [
+  const client = new vision.ImageAnnotatorClient();
+
+  const request = {
+    "requests": [
+      {
+        "image": {
+          "source": {
+            "imageUri": pathfile
+          }
+        },
+        "features": [
           {
-            "image": {
-              "source": {
-                "imageUri": pathfile
-              }
-            },
-            "features": [
-              {
-                "type": "TEXT_DETECTION"
-              }
-            ],
-            "imageContext": {
-              "languageHints": ["en"]
-            }
+            "type": "TEXT_DETECTION"
           }
-        ]
-      };
-    
-      const [result] = await client.batchAnnotateImages(request);
-      const detections = result.responses[0].fullTextAnnotation;
-      console.log(detections.text);
-    
-      await deep.insert({
-        type_id: await deep.id("@flakeed/google-vision", "PhotoTranscription"),
-        string: { data: { value: detections } },
-        in: {
-          data: {
-            type_id: await deep.id("@deep-foundation/core", "Contain"),
-            from_id: newLink.to_id
-          }
+        ],
+        "imageContext": {
+          "languageHints": ["en"]
         }
-      })
-    } 
-    finally {
-      fs.rmSync(keyFilePath, { recursive: true, force: true });
-    }
-    return detections;
+      }
+    ]
+  };
+
+  const [result] = await client.batchAnnotateImages(request);
+  detections = result.responses[0].fullTextAnnotation;
+
+  if (detections) {
+    console.log(detections.text);
+
+    await deep.insert({
+      type_id: await deep.id("@flakeed/google-vision", "PhotoTranscription"),
+      string: { data: { value: detections } },
+      in: {
+        data: {
+          type_id: await deep.id("@deep-foundation/core", "Contain"),
+          from_id: newLink.to_id
+        }
+      }
+    });
+  } else {
+    console.log("No text detected or error in the response.");
+  }
+} catch (error) {
+  console.error("Error processing image:", error);
+} finally {
+  fs.rmSync(keyFilePath, { recursive: true, force: true });
+}
+return detections;
 }
