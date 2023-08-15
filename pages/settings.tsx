c6a9a7838c82994a8a7be3ee21e6f14dc972eb52
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-15 23:33:36 +0600

    Update settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,22 +1,37 @@
-import { ChakraProvider, Link } from "@chakra-ui/react";
+import { ChakraProvider, Link, Stack } from "@chakra-ui/react";
 import { DeepClient, DeepProvider } from "@deep-foundation/deeplinks/imports/client";
 import { Provider } from "../imports/provider";
 import NextLink from 'next/link';
 import { Page } from "../components/page";
 
 
-function Content({deep, deviceLinkId}: {deep :DeepClient, deviceLinkId: number}) {
-   return <>
-             <div>
-            <Link as={NextLink} href="/settings/action-sheet">
-              Action Sheet
-            </Link>
-          </div>
-   </>
+function Content() {
+  return (
+    <Stack>
+      <Link as={NextLink} href="/settings/device">
+        Device
+      </Link>
+      <Link as={NextLink} href="/settings/motion">
+        Motion
+      </Link>
+      <Link as={NextLink} href="/settings/contact">
+        Contact
+      </Link>
+      <Link as={NextLink} href="/settings/network">
+        Network
+      </Link>
+      <Link as={NextLink} href="/settings/camera">
+        Voice Recorder
+      </Link>
+      <Link as={NextLink} href="/settings/camera">
+        Voice Recorder
+      </Link>
+    </Stack>
+  )
 }
 
 export default function SettingsPage() {
-   return (
-    <Page renderChildren={({deep,deviceLinkId}) => <Content deep={deep} deviceLinkId={deviceLinkId} />} />
-   );
- }
+  return (
+    <Page renderChildren={({ deep, deviceLinkId }) => <Content />} />
+  );
+}
