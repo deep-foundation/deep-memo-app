44a1cebd58e29ddfe023e9c237002a783b2f94de
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-18 04:00:53 +0600

    Add toasts

--- components/deep-memo/with-subscriptions.tsx
+++ components/deep-memo/with-subscriptions.tsx
@@ -4,6 +4,7 @@ import {
 import { saveAllCallHistory } from '../../imports/callhistory/callhistory';
 import { saveAllContacts } from '../../imports/contact/contact';
 import { useEffect } from 'react';
+import { useToast } from '@chakra-ui/react';
 
 export function WithSubscriptions({
    deep, 
@@ -28,20 +29,54 @@ export function WithSubscriptions({
       onLastContactsSyncTimeChange: (currentTime: number) => void,
       onLastCallHistorySyncTimeChange: (currentTime: number) => void,
      }) {
+      const toast = useToast();
 
   useEffect(() => {
     new Promise(async () => {
       const currentTime = new Date().getTime();
       if (isContactsSyncEnabled) {
         if (currentTime - lastContactsSyncTime) {
-          await saveAllContacts({ deep, deviceLinkId });
-          onLastContactsSyncTimeChange(currentTime);
+          try {
+            await saveAllContacts({ deep, deviceLinkId });
+            onLastContactsSyncTimeChange(currentTime);
+            toast({
+              title: 'Contacts synchronized successfully',
+              status: 'success',
+              duration: 5000,
+              isClosable: true,
+            })
+          } catch (error) {
+            toast({
+              title: 'Failed to synchronize contacts',
+              description: error.message,
+              status: 'error',
+              duration: null,
+              isClosable: true,
+            });
+          } 
+
         }
       }
       if (isCallHistorySyncEnabled) {
         if (currentTime - lastCallHistorySyncTime) {
-          await saveAllCallHistory({ deep, deviceLinkId });
-          onLastCallHistorySyncTimeChange(currentTime);
+          try {
+            await saveAllCallHistory({ deep, deviceLinkId });
+            onLastCallHistorySyncTimeChange(currentTime);
+            toast({
+              title: 'Call history synchronized successfully',
+              status: 'success',
+              duration: 5000,
+              isClosable: true,
+            })
+          } catch (error) {
+            toast({
+              title: 'Failed to synchronize call history',
+              description: error.message,
+              status: 'error',
+              duration: null,
+              isClosable: true,
+            });
+          }
         }
       }
       if (isNetworkSubscriptionEnabled) {
