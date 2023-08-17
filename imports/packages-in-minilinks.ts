d63bfef33f3eac6bc00ef3cc4fb63d9fd8bee516
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-18 03:33:23 +0600

    Add PackagesInMinilinks

---
+++ imports/packages-in-minilinks.ts
@@ -0,0 +1,7 @@
+export const PackagesInMinilinks = {
+  "@deep-foundation/core": "@deep-foundation/core",
+  "@deep-foundation/capacitor-device": "@deep-foundation/capacitor-device",
+  "@deep-foundation/capacitor-motion": "@deep-foundation/capacitor-motion",
+  "@deep-foundation/capacitor-network": "@deep-foundation/capacitor-network",
+  "@deep-foundation/capacitor-geolocation": "@deep-foundation/capacitor-geolocation",
+} as const;
