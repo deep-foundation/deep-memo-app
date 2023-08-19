4b8831ae5160f0f6a8e4a22dabd681814e260dcb
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-19 18:27:26 +0600

    Check if logger is installed and add button to install

--- pages/settings/logger.tsx
+++ pages/settings/logger.tsx
@@ -1,12 +1,16 @@
 import {
+  Button,
   Card,
   CardBody,
   CardHeader,
   ChakraProvider,
+  CircularProgress,
   FormControl,
   FormLabel,
   Heading,
   Switch,
+  Text,
+  VStack,
   useToast,
 } from '@chakra-ui/react';
 import { DeepClient, DeepProvider } from '@deep-foundation/deeplinks/imports/client';
@@ -17,10 +21,14 @@ import { Page } from '../../components/page';
 import { ChangeEvent, useState } from 'react';
 import { deepCopy } from '@firebase/util';
 import {SerialTransitionsBuilder} from '@deep-foundation/deeplinks/imports/experimental/serial-transitions-builder'
-import { PackagesInMinilinks } from '../../imports/packages-in-minilinks';
+import { REQUIRED_PACKAGES } from '../../imports/required-packages';
 import { SettingContent } from '../../components/setting-page';
 import { MutationInputLink } from '@deep-foundation/deeplinks/imports/client_types';
 import debug from 'debug'
+import { OPTIONAL_PACKAGES } from '../../imports/optional-packages';
+import { WithPackagesInstalled } from '@deep-foundation/react-with-packages-installed';
+import { ErrorAlert } from '../../components/error-alert';
+import { PackageManagementProxy } from '../../imports/package-management-proxy';
 
 function Content(options: ContentOptions) {
   const log = debug(`deep-foundation:pages:settings:logger:content`)
@@ -93,23 +101,23 @@ function Content(options: ContentOptions) {
           }
         })
       } else {
-        const handleInsertTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/core'], "HandleInsert")
-        const insertHandlerTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/logger'], "InsertHandler")
-        const handleUpdateTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/core'], "HandleUpdate")
-        const updateHandlerTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/logger'], "UpdateHandler")
-        const handleDeleteTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/core'], "HandleDelete")
-        const deleteHandlerTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/logger'], "DeleteHandler")
-        const deviceTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/capacitor-device'], "Device");
-        const motionTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/capacitor-motion'], "Motion");
-        const positionTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/capacitor-geolocation'], "Position");
-        const networkTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/capacitor-network'], "Network");
+        const handleInsertTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/core'], "HandleInsert")
+        const insertHandlerTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/logger'], "InsertHandler")
+        const handleUpdateTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/core'], "HandleUpdate")
+        const updateHandlerTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/logger'], "UpdateHandler")
+        const handleDeleteTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/core'], "HandleDelete")
+        const deleteHandlerTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/logger'], "DeleteHandler")
+        const deviceTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/capacitor-device'], "Device");
+        const motionTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/capacitor-motion'], "Motion");
+        const positionTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/capacitor-geolocation'], "Position");
+        const networkTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/capacitor-network'], "Network");
         const typesLinkIdsToLog = [
           deviceTypeLinkId,
           motionTypeLinkId,
           positionTypeLinkId,
           networkTypeLinkId
         ]
-        const containTypeLinkId = deep.idLocal(PackagesInMinilinks['@deep-foundation/core'], "Contain")
+        const containTypeLinkId = deep.idLocal(REQUIRED_PACKAGES['@deep-foundation/core'], "Contain")
         const insertData: Array<MutationInputLink> = typesLinkIdsToLog.flatMap(typeLinkId => [
           {
             type_id: handleInsertTypeLinkId,
@@ -187,9 +195,51 @@ function Content(options: ContentOptions) {
 
 
 export default function LoggerSettingsPage() {
+  const toast = useToast()
   return (
     <Page renderChildren={({deep,deviceLinkId}) => <SettingContent>
+    <WithPackagesInstalled
+    deep={deep}
+    packageNames={[OPTIONAL_PACKAGES['@deep-foundation/logger']]}
+    renderIfError={(error) => (
+      <ErrorAlert title={`Failed to check whether ${OPTIONAL_PACKAGES['@deep-foundation/logger']} is intalled`} description={error.message}/>
+    )}
+    renderIfLoading={() => (
+      <VStack height="100vh" justifyContent={"center"}>
+        <CircularProgress isIndeterminate />
+        <Text>Checking whether {OPTIONAL_PACKAGES['@deep-foundation/logger']} is installed...</Text>
+      </VStack>
+    )}
+    renderIfNotInstalled={() => (
+      <VStack>
+        <ErrorAlert title={`${OPTIONAL_PACKAGES['@deep-foundation/logger']} is not installed`} />
+        <Button onClick={async () => {
+          try {
+            const packageManagementProxy = new PackageManagementProxy(deep)
+          await packageManagementProxy.install(OPTIONAL_PACKAGES['@deep-foundation/logger'])
+          toast({
+            title: `Successfully installed ${OPTIONAL_PACKAGES['@deep-foundation/logger']}`,
+            status: "success",
+            duration: 5000,
+            isClosable: true
+          })
+          } catch (error) {
+            toast({
+              title: `Failed to install ${OPTIONAL_PACKAGES['@deep-foundation/logger']}`,
+              description: error.message,
+              status: "error",
+              duration: null,
+              isClosable: true
+            })
+          }
+        }}>
+          Install {OPTIONAL_PACKAGES['@deep-foundation/logger']}
+        </Button>
+      </VStack>
+    )}
+    >
     <Content deep={deep} />
+    </WithPackagesInstalled>
   </SettingContent>} />
   );
 }
