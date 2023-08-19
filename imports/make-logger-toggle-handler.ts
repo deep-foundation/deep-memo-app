365541c97ecd7f98335f1d5109673d43a1524572
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-19 18:43:04 +0600

    Add makeLoggerToggleHandler

---
+++ imports/make-logger-toggle-handler.ts
@@ -0,0 +1,31 @@
+import { ToastId, UseToastOptions } from "@chakra-ui/react";
+import { ChangeEvent } from "react";
+
+export async function makeLoggerToggleHandler(options: MakeLoggerToggleHandlerOptions) {
+  const { isLoggerEnabled, setIsLoggerEnabled, setIsLoading, toast } = options;
+  return async function toggleLoggerHandler(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+
+    setIsLoading(true)
+    try {
+      setIsLoggerEnabled(!isLoggerEnabled);
+    } catch (error) {
+      toast({
+        title: 'Failed to toggle logger',
+        description: error.message,
+        status: 'error',
+        duration: null,
+        isClosable: true,
+      })
+    } finally {
+      setIsLoading(false);
+    }
+  }
+};
+
+export interface MakeLoggerToggleHandlerOptions {
+  isLoggerEnabled: boolean;
+  setIsLoggerEnabled: (isLoggerEnabled: boolean) => void;
+  setIsLoading: (isLoading: boolean) => void;
+  toast: (options?: UseToastOptions) => ToastId
+}
