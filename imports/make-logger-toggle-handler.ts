c31721861705ba3d8340ade971492bb78d8483d9
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-20 18:11:27 +0600

    Add debugging

--- imports/make-logger-toggle-handler.ts
+++ imports/make-logger-toggle-handler.ts
@@ -2,10 +2,15 @@ import { ToastId, UseToastOptions } from "@chakra-ui/react";
 import { ChangeEvent, SyntheticEvent } from "react";
 import { toggleLogger } from "../components/toggle-logger";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import debug from "debug";
 
 export function makeLoggerToggleHandler(options: MakeLoggerToggleHandlerOptions) {
+  const log = debug(`deep-memo-app:${makeLoggerToggleHandler.name}`);
+  log({options})
   const { isLoggerEnabled, setIsLoggerEnabled, setIsLoading, toast, deep } = options;
   return async function toggleLoggerHandler(event: SyntheticEvent) {
+    const log = debug(`deep-memo-app:${toggleLoggerHandler.name}`);
+    log({event})
     event.preventDefault();
 
     setIsLoading(true)
@@ -20,6 +25,7 @@ export function makeLoggerToggleHandler(options: MakeLoggerToggleHandlerOptions)
           duration: null,
           isClosable: true,
         })
+        console.error(error)
       },
       onSuccess: () => {
         setIsLoggerEnabled(!isLoggerEnabled)
