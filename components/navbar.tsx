4a6eae4b667d1feebcfda070ae7a0168b01a4151
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-20 17:00:43 +0600

    Add LinkIcons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,15 @@
 import { HStack, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { LinkIcon } from '@chakra-ui/icons';
 
 export function NavBar({ } = {}) {
   return (
     <HStack>
     <Link as={NextLink} href='/'>
-      Home
+      Home <LinkIcon mx='2px' />
     </Link>
     <Link as={NextLink} href="/settings">
-        Settings
+        Settings <LinkIcon mx='2px' />
     </Link>
     </HStack>
   )
