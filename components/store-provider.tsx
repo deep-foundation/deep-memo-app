19e1a6699607987eede5ee7eb34936f6fd739ab4
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-21 11:28:43 +0600

    Add all providers

--- components/store-provider.tsx
+++ components/store-provider.tsx
@@ -1,13 +1,22 @@
 import { LocalStoreProvider } from "@deep-foundation/store/local";
 import { CapacitorStoreProvider } from "@deep-foundation/store/capacitor";
 import { QueryStoreProvider } from "@deep-foundation/store/query";
+import { CookiesStoreProvider } from "@deep-foundation/store/cookies";
 
-export function StoreProvider({ children }: { children: JSX.Element | JSX.Element[] }) {
-  return <QueryStoreProvider>
-    <LocalStoreProvider>
-    <CapacitorStoreProvider>
-      {children}
-    </CapacitorStoreProvider>
-    </LocalStoreProvider>
-  </QueryStoreProvider>
+export function StoreProvider({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  return (
+    <QueryStoreProvider>
+      <CookiesStoreProvider>
+        <LocalStoreProvider>
+          <CapacitorStoreProvider fetchInterval={5000}>
+            {children}
+          </CapacitorStoreProvider>
+        </LocalStoreProvider>
+      </CookiesStoreProvider>
+    </QueryStoreProvider>
+  );
 }
