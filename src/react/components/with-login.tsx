9ec1f36849ba8dd9ad26a10ba95a63f8d285cadf
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-24 16:25:02 +0600

    Update withLogin

--- src/react/components/with-login.tsx
+++ src/react/components/with-login.tsx
@@ -5,49 +5,59 @@ import { useToast } from "@chakra-ui/react";
 import { useLocalStore } from "@deep-foundation/store/local";
 import { CapacitorStoreKeys } from "../../capacitor-store-keys";
 import { processEnvs } from "../../process-envs";
+import { useToken } from "../hooks/use-token";
+import { useGqlPath } from "../hooks/use-gql-path";
 
-export function WithLogin({ gqlPath, setGqlPath, children }: { gqlPath: string | undefined, setGqlPath: (gqlPath: string | undefined) => void, children: JSX.Element }) {
+export function WithLogin({ children }: { children: JSX.Element }) {
   const toast = useToast();
   const deep = useDeep();
   const [isAuthorized, setIsAuthorized] = useState(undefined);
-  const [token, setToken] = useLocalStore(CapacitorStoreKeys[CapacitorStoreKeys.DeepToken], processEnvs.deepToken);
+  const [gqlPath, setGqlPath] = useGqlPath();
+  const [token, setToken] = useToken();
 
   useEffect(() => {
-    if(gqlPath || token) {
-      deep.login({token})
+    if (gqlPath || token) {
+      deep.login({ token });
     }
-  }, [token])
+  }, [token]);
 
   useEffect(() => {
     new Promise(async () => {
-      
-    self["deep"] = deep
-    if (deep.linkId !== 0) {
-      try {
-        await deep.select({id: 1})
-        setIsAuthorized(true);
-      } catch (error) {
-        setGqlPath(undefined);
-        await deep.logout();
-        toast({
-          title: "Login failed",
-          description: error.message,
-          status: "error",
-          duration: null,
-          isClosable: true,
-        })
+      if (deep.linkId === 0) {
+        setIsAuthorized(false);
+      } else {
+        try {
+          await deep.select({ id: 1 });
+          setIsAuthorized(true);
+        } catch (error) {
+          setGqlPath(undefined);
+          setToken(undefined);
+          await deep.logout();
+          toast({
+            title: "Login failed",
+            description: error.message,
+            status: "error",
+            duration: null,
+            isClosable: true,
+          });
+        }
       }
-    } else {
-      setIsAuthorized(false);
-    }
-    })
+    });
   }, [deep]);
 
-  return isAuthorized && gqlPath ? children : (
+  return isAuthorized ? (
+    children
+  ) : (
     <Login
       onSubmit={async (arg) => {
-        setGqlPath(arg.gqlPath);
-        setToken(arg.token)
+        const gqlPathUrl = new URL(arg.gqlPath);
+        setGqlPath(
+          gqlPathUrl.host +
+            gqlPathUrl.pathname +
+            gqlPathUrl.search +
+            gqlPathUrl.hash
+        );
+        setToken(arg.token);
       }}
     />
   );
