5feb48d1138c36dbf3d233c6eae6984ec9fbc079
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-27 17:10:15 +0600

    Show only button to enable all

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   Text,
   Link,
@@ -12,25 +12,25 @@ import {
   FormControl,
   FormLabel,
   Switch,
-} from '@chakra-ui/react';
-import {
-  DeepClient,
-} from '@deep-foundation/deeplinks/imports/client';
-import NextLink from 'next/link';
-import {LinkIcon} from '@chakra-ui/icons'
-import { Page } from '../src/react/components/page';
-import { CapacitorStoreKeys } from '../src/capacitor-store-keys';
-import { useLocalStore } from '@deep-foundation/store/local';
-import { SETTINGS_ROUTES } from '../src/settings-routes';
-import { capitalCase } from 'case-anything';
-import debug from 'debug';
-import { ErrorAlert } from '../src/react/components/error-alert';
-import { WithPackagesInstalled } from '@deep-foundation/react-with-packages-installed';
-import { OptionalPackages } from '../src/optional-packages';
-import { DecoratedDeep } from '../src/react/components/with-decorated-deep';
-import { WithSubscriptions } from '../src/react/components/with-subscriptions';
-import { NavBar } from '../src/react/components/navbar';
-import { Monitoring } from '../src/react/components/monitoring';
+} from "@chakra-ui/react";
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import NextLink from "next/link";
+import { LinkIcon } from "@chakra-ui/icons";
+import { Page } from "../src/react/components/page";
+import { CapacitorStoreKeys } from "../src/capacitor-store-keys";
+import { useLocalStore } from "@deep-foundation/store/local";
+import { SETTINGS_ROUTES } from "../src/settings-routes";
+import { capitalCase } from "case-anything";
+import debug from "debug";
+import { ErrorAlert } from "../src/react/components/error-alert";
+import { WithPackagesInstalled } from "@deep-foundation/react-with-packages-installed";
+import { OptionalPackages } from "../src/optional-packages";
+import { DecoratedDeep } from "../src/react/components/with-decorated-deep";
+import { WithSubscriptions } from "../src/react/components/with-subscriptions";
+import { NavBar } from "../src/react/components/navbar";
+import { Monitoring } from "../src/react/components/monitoring";
+import { setAllDataSync } from "../src/set-all-data-sync";
+import { useIsAllDataSyncEnabled } from "../src/react/hooks/use-is-all-data-sync-enabled";
 
 interface ContentParam {
   deep: DecoratedDeep;
@@ -39,7 +39,7 @@ interface ContentParam {
 
 function Content({ deep, deviceLinkId }: ContentParam) {
   useEffect(() => {
-    import('@ionic/pwa-elements/loader').then(({ defineCustomElements }) => {
+    import("@ionic/pwa-elements/loader").then(({ defineCustomElements }) => {
       defineCustomElements(window);
     });
   }, []);
@@ -53,58 +53,51 @@ function Content({ deep, deviceLinkId }: ContentParam) {
     });
   }, [deep]);
 
-  const [isContactsSyncEnabled, setIsContactsSyncEnabled] = useLocalStore<boolean|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled],
-    undefined
-  );
-  const [lastContactsSyncTime, setLastContactsSyncTime] = useLocalStore<number|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.ContactsLastSyncTime],
-    undefined
-  );
-  const [isCallHistorySyncEnabled, setIsCallHistorySyncEnabled] = useLocalStore<boolean|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled],
-    undefined
-  );
-  const [lastCallHistorySyncTime, setLastCallHistorySyncTime] = useLocalStore<number | undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.CallHistoryLastSyncTime],
-    undefined
-  );
-  const [isNetworkSyncEnabled, setIsNetworkSyncEnabled] =
-    useLocalStore<boolean|undefined>(
-      CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled],
-      false
-    );
-  const [isVoiceRecorderEnabled, setIsVoiceRecorderEnabled] = useLocalStore<boolean|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled],
-    undefined
-  );
+  const [isContactsSyncEnabled, setIsContactsSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled], undefined);
+  const [lastContactsSyncTime, setLastContactsSyncTime] = useLocalStore<
+    number | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.ContactsLastSyncTime], undefined);
+  const [isCallHistorySyncEnabled, setIsCallHistorySyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled], undefined);
+  const [lastCallHistorySyncTime, setLastCallHistorySyncTime] = useLocalStore<
+    number | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.CallHistoryLastSyncTime], undefined);
+  const [isNetworkSyncEnabled, setIsNetworkSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled], false);
+  const [isVoiceRecorderEnabled, setIsVoiceRecorderEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled], undefined);
   const [isLoggerEnabled, setIsLoggerEnabled] = useLocalStore(
     CapacitorStoreKeys[CapacitorStoreKeys.IsLoggerEnabled],
     undefined
   );
-  const [isMotionSyncEnabled, setIsMotionSyncEnabled] = useLocalStore<boolean|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled],
-    undefined
-  );
-  const [isGeolocationSyncEnabled, setIsGeolocationSyncEnabled] = useLocalStore<boolean|undefined>(
-    CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled],
-    undefined
-  );
+  const [isMotionSyncEnabled, setIsMotionSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled], undefined);
+  const [isGeolocationSyncEnabled, setIsGeolocationSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled], undefined);
 
-  
+  const isAllDataSyncEnabled = useIsAllDataSyncEnabled();
 
   return (
-    <Stack alignItems={'center'}>
+    <Stack alignItems={"center"}>
       <NavBar />
-      <Heading as={'h1'}>DeepMemo</Heading>
-      <Button onClick={async () => {
-        await deep.updateDevice({
-          deviceLinkId
-        })
-      }}>
+      <Heading as={"h1"}>DeepMemo</Heading>
+      <Button
+        onClick={async () => {
+          await deep.updateDevice({
+            deviceLinkId,
+          });
+        }}
+      >
         Update Device Info
       </Button>
-      <WithSubscriptions 
+      <WithSubscriptions
         deep={deep}
         deviceLinkId={deviceLinkId}
         isContactsSyncEnabled={isContactsSyncEnabled}
@@ -117,86 +110,84 @@ function Content({ deep, deviceLinkId }: ContentParam) {
         isVoiceRecorderEnabled={isVoiceRecorderEnabled}
         isMotionSyncEnabled={isMotionSyncEnabled}
         isGeolocationSyncEnabled={isGeolocationSyncEnabled}
-        />
-        {
-          isLoggerEnabled ? (
-            <WithPackagesInstalled
-            deep={deep}
-            packageNames={[OptionalPackages.Logger]}
-            renderIfError={(error) => (
-              <VStack>
-                <ErrorAlert title="Error checking whether logger is installed" description={error.message} />
-              </VStack>
-            )}
-            renderIfLoading={() => (
-              <VStack>
-                <Text>Checking whether logger installed...</Text>
-              </VStack>
-            )}
-            renderIfNotInstalled={() => {
-              setIsLoggerEnabled(false)
-              return (
-                <VStack>
-                  <ErrorAlert title="Logger is not installed" description={
-                    <VStack>
-                      <Text>Disable logger in settings and then install it. Note: if you disable logger in settings you will see installation button</Text>
-                      <Link as={NextLink} href="/settings/logger">
-                        Logger Settings <LinkIcon mx='2px' />
-                        </Link>
-                      </VStack>
-                  } />
-                </VStack>
-              )
-            }}
-            >
-                    <Card>
-          <CardHeader>
-            <Heading>Call History</Heading>
-          </CardHeader>
-          <CardBody>
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="sync-call-history-switch" mb="0">
-                Synchronize All Data
-              </FormLabel>
-              <Switch
-                id="sync-call-history-switch"
-                isChecked={
-                  isContactsSyncEnabled &&
-                  isCallHistorySyncEnabled &&
-                  isMotionSyncEnabled &&
-                  isGeolocationSyncEnabled &&
-                  isNetworkSyncEnabled &&
-                  isVoiceRecorderEnabled &&
-                  isLoggerEnabled
-                }
-                onChange={(event) => {
-                  setIsContactsSyncEnabled(event.target.checked);
-                  setIsCallHistorySyncEnabled(event.target.checked);
-                  setIsMotionSyncEnabled(event.target.checked);
-                  setIsGeolocationSyncEnabled(event.target.checked);
-                  setIsNetworkSyncEnabled(event.target.checked);
-                  setIsVoiceRecorderEnabled(event.target.checked);
-                  setIsLoggerEnabled(event.target.checked);
-                }}
-              />
-            </FormControl>
-          </CardBody>
-        </Card>
-              {/* <Monitoring deep={deep} isLoggerEnabled={isLoggerEnabled} deviceLinkId={deviceLinkId} /> */}
-            </WithPackagesInstalled>
-          ) : (
+      />
+      <Card>
+        <CardHeader>
+          <Heading>Data Synchronization</Heading>
+        </CardHeader>
+        <CardBody>
+          <FormControl display="flex" alignItems="center">
+            <FormLabel htmlFor="sync-call-history-switch" mb="0">
+              Synchronize All Data
+            </FormLabel>
+            <Switch
+              id="sync-call-history-switch"
+              isChecked={isAllDataSyncEnabled}
+              onChange={(event) => {
+                setAllDataSync(event.target.checked)
+              }}
+            />
+          </FormControl>
+        </CardBody>
+      </Card>
+      {/* {isLoggerEnabled ? (
+        <WithPackagesInstalled
+          deep={deep}
+          packageNames={[OptionalPackages.Logger]}
+          renderIfError={(error) => (
             <VStack>
-            <ErrorAlert title="Logger is disabled" description={
+              <ErrorAlert
+                title="Error checking whether logger is installed"
+                description={error.message}
+              />
+            </VStack>
+          )}
+          renderIfLoading={() => (
+            <VStack>
+              <Text>Checking whether logger installed...</Text>
+            </VStack>
+          )}
+          renderIfNotInstalled={() => {
+            setIsLoggerEnabled(false);
+            return (
+              <VStack>
+                <ErrorAlert
+                  title="Logger is not installed"
+                  description={
+                    <VStack>
+                      <Text>
+                        Disable logger in settings and then install it. Note: if
+                        you disable logger in settings you will see installation
+                        button
+                      </Text>
+                      <Link as={NextLink} href="/settings/logger">
+                        Logger Settings <LinkIcon mx="2px" />
+                      </Link>
+                    </VStack>
+                  }
+                />
+              </VStack>
+            );
+          }}
+        >
+          
+          <Monitoring deep={deep} isLoggerEnabled={isLoggerEnabled} deviceLinkId={deviceLinkId} />
+        </WithPackagesInstalled>
+      ) : (
+        <VStack>
+          <ErrorAlert
+            title="Logger is disabled"
+            description={
               <VStack>
                 <Text>Enable the logger to see logs</Text>
                 <Link as={NextLink} href="/settings/logger">
-                  Logger Settings <LinkIcon mx='2px' />
-                  </Link>
-                </VStack>
-            } />
-          </VStack>
-          )
-        }
+                  Logger Settings <LinkIcon mx="2px" />
+                </Link>
+              </VStack>
+            }
+          />
+        </VStack>
+      )} */}
     </Stack>
   );
 }
@@ -214,13 +205,11 @@ export default function IndexPage() {
 function Pages() {
   return (
     <Stack>
-      {
-        Object.entries(SETTINGS_ROUTES).map(([name, route]) => (
-          <Link as={NextLink} href={route}>
-            {capitalCase(name)} <LinkIcon mx='2px' />
-          </Link>
-        ))
-      }
+      {Object.entries(SETTINGS_ROUTES).map(([name, route]) => (
+        <Link as={NextLink} href={route}>
+          {capitalCase(name)} <LinkIcon mx="2px" />
+        </Link>
+      ))}
     </Stack>
   );
 }
