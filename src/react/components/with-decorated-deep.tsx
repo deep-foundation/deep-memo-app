46f9f77ec70dac7b392ab39b57ecd5f61e55066e
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-27 16:40:14 +0600

    Add more decorators

--- src/react/components/with-decorated-deep.tsx
+++ src/react/components/with-decorated-deep.tsx
@@ -1,5 +1,6 @@
 import { DeviceDecorator, createDeviceDecorator } from "@deep-foundation/capacitor-device";
 import { GeolocationDecorator, createGeolocationDecorator } from "@deep-foundation/capacitor-geolocation";
+import { MotionDecorator, createMotionDecorator } from "@deep-foundation/capacitor-motion";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client.js";
 import { compose } from 'lodash/fp';
 import { useEffect } from "react";
@@ -7,7 +8,8 @@ import { useEffect } from "react";
 export function WithDecoratedDeep(options: WithDecoratedDeepOptions) {
   const { deep, renderChildren } = options;
   const deviceDecoratedDeep = createDeviceDecorator(deep);
-  const geolocationDecoratedDeep = createGeolocationDecorator(deviceDecoratedDeep);
+  const motionDecoratedDeep = createMotionDecorator(deviceDecoratedDeep);
+  const geolocationDecoratedDeep = createGeolocationDecorator(motionDecoratedDeep);
   const decoratedDeep = geolocationDecoratedDeep;
   useEffect(() => {
     self['decoratedDeep'] = decoratedDeep;
@@ -22,4 +24,4 @@ export interface WithDecoratedDeepOptions {
 
 export interface RenderChildrenOptions {deep: DecoratedDeep}
 
-export type DecoratedDeep = GeolocationDecorator<DeepClient> & DeviceDecorator<DeepClient>;
+export type DecoratedDeep = GeolocationDecorator<DeepClient> & DeviceDecorator<DeepClient> & MotionDecorator<DeepClient>;
