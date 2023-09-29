d1452b9bd4d57894a423f6f5cf66f66af3c62582
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-29 11:48:06 +0600

    Add more logs

--- src/react/hooks/use-is-all-data-sync-enabled.ts
+++ src/react/hooks/use-is-all-data-sync-enabled.ts
@@ -1,41 +1,52 @@
 import { useLocalStore } from "@deep-foundation/store/local";
 import { CapacitorStoreKeys } from "../../capacitor-store-keys";
+import { packageLog } from "../../package-log";
 
 export function useIsAllDataSyncEnabled() {
+  const log = packageLog.extend(useIsAllDataSyncEnabled.name);
   const [isContactsSyncEnabled, setIsContactsSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled], undefined);
-const [lastContactsSyncTime, setLastContactsSyncTime] = useLocalStore<
-  number | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.ContactsLastSyncTime], undefined);
-const [isCallHistorySyncEnabled, setIsCallHistorySyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled], undefined);
-const [lastCallHistorySyncTime, setLastCallHistorySyncTime] = useLocalStore<
-  number | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.CallHistoryLastSyncTime], undefined);
-const [isNetworkSyncEnabled, setIsNetworkSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled], false);
-const [isVoiceRecorderEnabled, setIsVoiceRecorderEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled], undefined);
-const [isMotionSyncEnabled, setIsMotionSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled], undefined);
-const [isGeolocationSyncEnabled, setIsGeolocationSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled], undefined);
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled], undefined);
+  log({ isContactsSyncEnabled, setIsContactsSyncEnabled });
+  const [lastContactsSyncTime, setLastContactsSyncTime] = useLocalStore<
+    number | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.ContactsLastSyncTime], undefined);
+  log({ lastContactsSyncTime, setLastContactsSyncTime });
+  const [isCallHistorySyncEnabled, setIsCallHistorySyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled], undefined);
+  log({ isCallHistorySyncEnabled, setIsCallHistorySyncEnabled });
+  const [lastCallHistorySyncTime, setLastCallHistorySyncTime] = useLocalStore<
+    number | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.CallHistoryLastSyncTime], undefined);
+  log({ lastCallHistorySyncTime, setLastCallHistorySyncTime });
+  const [isNetworkSyncEnabled, setIsNetworkSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled], false);
+  log({ isNetworkSyncEnabled, setIsNetworkSyncEnabled });
+  const [isVoiceRecorderEnabled, setIsVoiceRecorderEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled], undefined);
+  log({ isVoiceRecorderEnabled, setIsVoiceRecorderEnabled });
+  const [isMotionSyncEnabled, setIsMotionSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled], undefined);
+  log({ isMotionSyncEnabled, setIsMotionSyncEnabled });
+  const [isGeolocationSyncEnabled, setIsGeolocationSyncEnabled] = useLocalStore<
+    boolean | undefined
+  >(CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled], undefined);
+  log({ isGeolocationSyncEnabled, setIsGeolocationSyncEnabled });
 
-const isAllDataSyncEnabled =
-  isContactsSyncEnabled &&
-  lastContactsSyncTime &&
-  isCallHistorySyncEnabled &&
-  lastCallHistorySyncTime &&
-  isNetworkSyncEnabled &&
-  isVoiceRecorderEnabled &&
-  isMotionSyncEnabled &&
-  isGeolocationSyncEnabled;
+  const isAllDataSyncEnabled =
+    isContactsSyncEnabled &&
+    lastContactsSyncTime &&
+    isCallHistorySyncEnabled &&
+    lastCallHistorySyncTime &&
+    isNetworkSyncEnabled &&
+    isVoiceRecorderEnabled &&
+    isMotionSyncEnabled &&
+    isGeolocationSyncEnabled;
+  log({ isAllDataSyncEnabled });
 
   return isAllDataSyncEnabled;
 }
