87be68d0748d0bb9d8c9f9f50d3a4fe3fb1a43a3
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-29 11:45:36 +0600

    Do not use setAllDataSync as hook

--- src/set-all-data-sync.ts
+++ src/set-all-data-sync.ts
@@ -1,30 +1,16 @@
-import { useLocalStore } from "@deep-foundation/store/local";
 import { CapacitorStoreKeys } from "./capacitor-store-keys";
 
 export function setAllDataSync(toggle: boolean) {
-  const [isContactsSyncEnabled, setIsContactsSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled], undefined);
-const [isCallHistorySyncEnabled, setIsCallHistorySyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled], undefined);
-const [isNetworkSyncEnabled, setIsNetworkSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled], false);
-const [isVoiceRecorderEnabled, setIsVoiceRecorderEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled], undefined);
-const [isMotionSyncEnabled, setIsMotionSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled], undefined);
-const [isGeolocationSyncEnabled, setIsGeolocationSyncEnabled] = useLocalStore<
-  boolean | undefined
->(CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled], undefined);
+  const allKeys = [
+    CapacitorStoreKeys.IsContactsSyncEnabled,
+    CapacitorStoreKeys.IsCallHistorySyncEnabled,
+    CapacitorStoreKeys.IsNetworkSubscriptionEnabled,
+    CapacitorStoreKeys.IsVoiceRecorderEnabled,
+    CapacitorStoreKeys.IsMotionSyncEnabled,
+    CapacitorStoreKeys.IsGeolocationSyncEnabled,
+  ];
 
-setIsContactsSyncEnabled(toggle);
-setIsCallHistorySyncEnabled(toggle);
-setIsNetworkSyncEnabled(toggle);
-setIsVoiceRecorderEnabled(toggle);
-setIsMotionSyncEnabled(toggle);
-setIsGeolocationSyncEnabled(toggle);
+  allKeys.forEach((key) => {
+    localStorage.setItem(CapacitorStoreKeys[key], JSON.stringify(toggle));
+  });
 }
