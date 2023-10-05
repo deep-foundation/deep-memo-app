dec1fbb93551222d83d81a3afb3e21d7c768318b
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-05 23:53:14 +0600

    Enable data sync only if permission is granted

--- src/set-all-data-sync.ts
+++ src/set-all-data-sync.ts
@@ -1,22 +1,40 @@
 import { CapacitorStoreKeys } from "./capacitor-store-keys";
 import { Preferences } from "@capacitor/preferences";
+import {checkPermissions as checkGeolocationPermissions} from '@deep-foundation/capacitor-geolocation'
+import {checkVoiceRecorderPermissions} from '@deep-foundation/capacitor-voice-recorder'
 
 export async function setAllDataSync(toggle: boolean) {
-  const allKeys = [
-    CapacitorStoreKeys.IsContactsSyncEnabled,
-    CapacitorStoreKeys.IsCallHistorySyncEnabled,
-    CapacitorStoreKeys.IsNetworkSubscriptionEnabled,
-    CapacitorStoreKeys.IsVoiceRecorderEnabled,
-    CapacitorStoreKeys.IsMotionSyncEnabled,
-    CapacitorStoreKeys.IsGeolocationSyncEnabled,
-  ];
+
+  const packages = [
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsContactsSyncEnabled],
+      arePermissionsGranted: true,
+    },
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsGeolocationSyncEnabled],
+      arePermissionsGranted: await checkGeolocationPermissions(),
+    },
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsCallHistorySyncEnabled],
+      arePermissionsGranted: true,
+    },
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsNetworkSubscriptionEnabled],
+      arePermissionsGranted: true,
+    },
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsVoiceRecorderEnabled],
+      arePermissionsGranted: await checkVoiceRecorderPermissions(),
+    },
+    {
+      storeKey: CapacitorStoreKeys[CapacitorStoreKeys.IsMotionSyncEnabled],
+      arePermissionsGranted: true,
+    },
+  ]
 
   await Promise.all(
-    allKeys.map(async (key) =>
-      Preferences.set({
-        key: CapacitorStoreKeys[key],
-        value: JSON.stringify(toggle),
-      })
+    packages.map(async (_package) =>
+      _package.arePermissionsGranted ? Preferences.set({ key: _package.storeKey, value: JSON.stringify(toggle) }) : undefined
     )
   );
 }
