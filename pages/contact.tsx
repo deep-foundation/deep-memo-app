89df22cfe3e56168dcb837e8856c835e961b35ea
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-05 21:14:30 +0600

    Remove unused pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,17 +3,17 @@ import {
   DeepClient,
 } from '@deep-foundation/deeplinks/imports/client';
 import { Button, Stack } from '@chakra-ui/react';
-import { saveAllContacts } from '../src/contact/contact';
 import { Page } from '../src/react/components/page';
 import { NavBar } from '../src/react/components/navbar';
 
 function Content({deep, deviceLinkId}: {deep :DeepClient, deviceLinkId: number}) {
-  return (
-    <Stack>
-      <NavBar/>
-      <Button onClick={() => saveAllContacts({ deep, deviceLinkId })}>Save All Contacts</Button>
-    </Stack>
-  );
+  return null
+  // return (
+  //   <Stack>
+  //     <NavBar/>
+  //     <Button onClick={() => saveAllContacts({ deep, deviceLinkId })}>Save All Contacts</Button>
+  //   </Stack>
+  // );
 }
 
 export default function ContactsPage() {
