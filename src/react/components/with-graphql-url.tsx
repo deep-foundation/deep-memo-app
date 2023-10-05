45be3a9fe244ec968d505546a9d3cfdaf4f2f150
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-05 21:26:36 +0600

    Add types and logic

--- src/react/components/with-graphql-url.tsx
+++ src/react/components/with-graphql-url.tsx
@@ -1,13 +1,19 @@
 import { useGraphQlUrl } from "../hooks/use-gql-path";
 
 export function WithGraphQlUrl(options: WithGraphQlUrlOptions) {
-  const [graphQlUrl, setGraphQlUrl] = useGraphQlUrl();
-  return options.renderChildren({
-    graphQlUrl,
-    setGraphQlUrl
-  });
+  const [graphQlUrl, setGraphQlUrl, unsertGraphQlUrl, isLoading] = useGraphQlUrl();
+  if(isLoading) {
+    return options.renderIfLoading()
+  } else {
+    return options.renderChildren({
+      graphQlUrl,
+      setGraphQlUrl
+    });
+  }
+  
 }
 
 export interface WithGraphQlUrlOptions {
-  renderChildren: (param: { graphQlUrl: string, setGraphQlUrl: (newGraphQl: string) => void }) => JSX.Element;
+  renderChildren: (param: { graphQlUrl: string|undefined, setGraphQlUrl: (newGraphQl: string) => void }) => JSX.Element;
+  renderIfLoading: () => JSX.Element;
 }
