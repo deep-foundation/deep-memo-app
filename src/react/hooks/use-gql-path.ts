32a5fc9804c4261354cf8a0f3ace5a6b8eafa9a3
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-05 21:24:08 +0600

    Fix error

--- src/react/hooks/use-gql-path.ts
+++ src/react/hooks/use-gql-path.ts
@@ -2,6 +2,6 @@ import { useCapacitorStore } from "@deep-foundation/store/capacitor";
 import { processEnvs } from "../../process-envs";
 import { CapacitorStoreKeys } from "../../capacitor-store-keys";
 
-export function useGraphQlUrl(defaultValue: string = processEnvs.graphQlUrl) {
+export function useGraphQlUrl(defaultValue: string|undefined = processEnvs.graphQlUrl) {
   return useCapacitorStore(CapacitorStoreKeys[CapacitorStoreKeys.GraphQlUrl], defaultValue)
 }
