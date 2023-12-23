99a8f93b6b9594ac15e478633d0c4f1e5b0cac0e
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-23 16:09:32 +0600

    Uncomment geolocation code

--- src/react/components/with-sync.tsx
+++ src/react/components/with-sync.tsx
@@ -124,7 +124,7 @@ export function WithSync(options: {
         // isNetworkSyncEnabled && <NetworkStatus deep={deep} containerLinkId={deviceLinkId} />
       }
       {
-        // isGeolocationSyncEnabled && <WithPositionSync containerLinkId={deviceLinkId} deep={deep}/>
+        isGeolocationSyncEnabled && <WithPositionSync containerLinkId={deviceLinkId} deep={deep}/>
       }
       {
         // isVoiceRecorderEnabled && <WithRecording deep={deep} containerLinkId={deviceLinkId} savingIntervalInMs={10*1000} renderIfError={(error) => (
